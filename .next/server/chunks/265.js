690722206f603feb593fe4b9fa8ba412e358c69b
Diego Tenório <38768837+diegoptenorio@users.noreply.github.com>
2022-02-15 02:41:53 -0300

    relative path to tailwind

---
+++ .next/server/chunks/265.js
@@ -0,0 +1,70 @@
+"use strict";
+exports.id = 265;
+exports.ids = [265];
+exports.modules = {
+
+/***/ 618:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/tonguemeleon_0.c5d5ed4f.svg","height":400,"width":1074});
+
+/***/ }),
+
+/***/ 265:
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.a(module, async (__webpack_handle_async_dependencies__) => {
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "r": () => (/* binding */ news),
+/* harmony export */   "jZ": () => (/* binding */ about),
+/* harmony export */   "yN": () => (/* binding */ releases),
+/* harmony export */   "PX": () => (/* binding */ contact)
+/* harmony export */ });
+/* harmony import */ var uuid__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(555);
+/* harmony import */ var _assets_img_articles_tonguemeleon_0_svg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(618);
+var __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([uuid__WEBPACK_IMPORTED_MODULE_0__]);
+uuid__WEBPACK_IMPORTED_MODULE_0__ = (__webpack_async_dependencies__.then ? await __webpack_async_dependencies__ : __webpack_async_dependencies__)[0];
+
+
+const news = [
+    {
+        id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v1)(),
+        image: _assets_img_articles_tonguemeleon_0_svg__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z,
+        title: 'Tonguemeleon in closed tests',
+        text: "Still don't know Tonguemeleon? Get ready to control a hungry chameleon in this casual mobile game and show your full potential by catching the bugs that fly over the lake. Attention because some mysterious bugs can awaken special abilities in our friend Tonguemeleon."
+    }
+];
+const about = [
+    {
+        id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v1)(),
+        image: _assets_img_articles_tonguemeleon_0_svg__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z,
+        title: 'About Cyan Storm',
+        text: ""
+    }
+];
+const releases = [
+    {
+        id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v1)(),
+        image: _assets_img_articles_tonguemeleon_0_svg__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z,
+        title: 'Releases',
+        text: ""
+    }
+];
+const contact = [
+    {
+        id: (0,uuid__WEBPACK_IMPORTED_MODULE_0__.v1)(),
+        image: _assets_img_articles_tonguemeleon_0_svg__WEBPACK_IMPORTED_MODULE_1__/* ["default"] */ .Z,
+        title: 'Contact',
+        text: ""
+    }
+];
+
+});
+
+/***/ })
+
+};
+;
